03e5ac66bd83c7f875ee0c1bfb8b2cd2ed380177
T4peduck <05jthill@gmail.com>
2025-02-23 17:29:55 -0700

    testing bufferReader class

--- MusicStore.java
+++ MusicStore.java
@@ -4,10 +4,25 @@
  * Represents something
  * 
  * TODO: fix comments at top
+ * TODO: do uml
  */
 
- public class MusicStore {
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.lang.String;
 
+public class MusicStore {
+	 private ArrayList<Album> AlbumList;
+	 private BufferedReader br;
+	 
+	 public MusicStore() {
+		 AlbumList = new ArrayList<Album>();
+	 }
+	 
+	 public void parseFile(new FileReader()) {
+		 
+		 
+	 }
 
 
  }
---
+++ main.java
@@ -0,0 +1,31 @@
+/*
+ * simulates the program
+ */
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.File;
+import java.util.Arrays;
+
+
+public class main {
+	public static void main(String args[]) throws IOException {
+		File dir = new File("albums");
+		System.out.println(Arrays.toString(dir.listFiles()));
+		
+		BufferedReader br;
+		
+		
+		for(File file: dir.listFiles()) {
+			String line;
+			br = new BufferedReader(new FileReader(file));
+			
+			while((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+			System.out.println();
+		}
+		
+		
+	}
+}
