d1cb604d9bad08e9105df6809617f61c8f6d0320
eecushner <166892587+eecushner@users.noreply.github.com>
2025-02-28 22:02:48 -0700

    Update view.java

--- view.java
+++ view.java
@@ -86,6 +86,20 @@ public class view {
 					System.out.println("Error: Invalid Input");
 				}			
 			}
+			else if(command.equals("rate")) {
+				try{
+					String secondInput = input[2];
+				}
+				catch(ArrayIndexOutOfBoundsException e) {
+					System.out.println("Error: Invalid Input");
+				}
+				try {
+					rate(input[1], Integer.parseInt(input[2]));
+				}
+				catch(NumberFormatException e) {
+					System.out.println("Error: Invalid Input");
+				}
+			}
 			else if(command.equals("removepl")) {
 				try{
 					removePL(input[1], input[2]);
