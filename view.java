eb100d8e2e43bf42679f4fc9d385d71c2334fc01
T4peduck <05jthill@gmail.com>
2025-03-25 00:07:22 -0700

    sadface

--- view.java
+++ view.java
@@ -413,7 +413,7 @@ public class view {
 		while(!done) {
 			username = keyboard.nextLine();
 			if(usernames.contains(username))
-				System.out.println("Error: An account with this name already exists. Please enter a different username: ");
+				System.out.print("Error: An account with this name already exists. Please enter a different username: ");
 			 else
 				 done = true;
 		}
@@ -431,7 +431,7 @@ public class view {
 				validPassword = false;
 			}
 			if(!validPassword) {
-				System.out.println("Error: Passwords can include only letters or digits, no spaces, and must be of appropriate length.\n Please enter a valid password:");
+				System.out.print("Error: Passwords can include only letters or digits, no spaces, and must be of appropriate length.\n Please enter a valid password:");
 			}
 			else
 				done = true;
@@ -683,7 +683,7 @@ public class view {
 		ArrayList<String> songs = ul.getSongs();
 		for(String s : songs) {
 			if(s.toLowerCase().equals(argument.toLowerCase())) {
-				ul.rateSong(argument, "artist",  rating);
+				ul.rateSong(argument,  rating);
 				return;
 			}
 		}
